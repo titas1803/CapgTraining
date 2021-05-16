ec6316868f726c35223d07c25344c2fe921c436c
titas1803 <titas.sarkar420@hotmail.com>
2021-05-16 19:23:19 +0530

    update 1

--- SpringBootRest/src/main/java/com/cg/exceptions/EmpNotFoundException.java
+++ SpringBootRest/src/main/java/com/cg/exceptions/EmpNotFoundException.java
@@ -4,12 +4,10 @@ public class EmpNotFoundException extends Exception{
 
 	public EmpNotFoundException() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	public EmpNotFoundException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
 	
